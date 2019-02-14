a5b28108d77151bf5493a761dfe799c558134da0
RyanBeal7 <ryan.beal@soton.ac.uk>
2019-02-14 10:46:52 +0000

    Update Main.java

--- FPL_CPLEX_Optimiser/src/Main.java
+++ FPL_CPLEX_Optimiser/src/Main.java
@@ -4,8 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Test t = new Test();
-
         System.out.println("-------- CPLEX EXAMPLE -------- ");
 
         DataReader dr = new DataReader();
@@ -13,7 +11,7 @@ public class Main {
 
         //Optimiser fplMain = new Optimiser(dr.getPlayers(), "1-3-4-3", 820);
         //Optimiser fplBench = new Optimiser(dr.getPlayers(), "1-1-1-1", 180);
-        Optimiser fplAll = new Optimiser(dr.getPlayers(), "0-0-1-1", 118);
+        Optimiser fplAll = new Optimiser(dr.getPlayers(), "2-5-5-3", 1000);
 
         try {
             //fplMain.solve();
